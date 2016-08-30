057d4f2faac6e3f28e9eb0335b8cf32ec124bc26
DexCoulson <NoahCardoza@users.noreply.github.com>
2016-08-30 15:35:22 -0700

    internet.php
    + internet.php
    + internet.css
    + templets ¬
    + navbar.php
    + .button-normal (main.css)

---
+++ htdocs/templets/navbar.php
@@ -0,0 +1,10 @@
+<div class="main-menu">
+        <div class="main-menu-option">News</div>
+        <div class="main-menu-option-selected">Hack</div>
+        <div class="main-menu-option">E-Mail</div>
+        <div class="main-menu-option">Finances</div>
+        <div class="main-menu-option">Database</div>
+        <div class="main-menu-option">Clan</div>
+        <div class="main-menu-option">Leaderboards</div>
+        <div class="main-menu-option">Profile</div>
+    </div>

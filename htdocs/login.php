7467e8818d2327cb280fbc069534272b432459c3
Wiguwbe <tiago.t@sapo.pt>
2016-09-30 20:46:21 +0100

    Update login.php

--- htdocs/login.php
+++ htdocs/login.php
@@ -2,32 +2,21 @@
     require_once 'dbconnect.php';
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if(isset($username) && !empty($username) && isset($password) && !empty($password)) {
-        $table  = ""; // Table name (Users?)
-        $sql    = "SELECT password FROM $table WHERE username = $username;"; // Need implement SQL-injection defenses
-        $result = $conn->query($sql);
-        if($result->num_rows == 1) {
-            // Compare (possibly hashed) passwords
-            $row = $result->fetch_assoc();
-            if(strcmp($password,row["password"]) {
-                $_SESSION["username"] = $username;
-                $_SESSION["auth"] = true;
-                //Redirect somewhere
-            }
-            else {
-                //Return something
-            }
-        }
-        else {
-            if($result->num_rows == 0) {
-                // Create user
-                // Redirect to register page
-            }
-            else /*More than one row*/ {
-                //More than one user per username(?)
-                //Possible SQL injection(?)
-                // Maybe notify game admins
-            }
-        }
-    }
+    if(isset($username) && !empty($username) && isset($password) && !empty($password))
+    {
+        $table = 'users';
+        $sql=$conn->prepare("SELECT UserName, UserPassword, Banned FROM $table WHERE UserName ='$username'");
+        $sql->execute();
+        
+        $result = $sql->setFetchMode(PDO::FETCH_ASSOC);
+        $lines = $sql->fetchAll();
+        switch(count($lines))
+        {
+            case 0:
+                //New user
+                //Redirect to register page
+                break;
+            case 1:
+                // One single user
+                if(
 ?>
